80f573ae3b78ab02b2d95c5c4e46a3608f8b575e
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-11-12 20:23:25 +0200

    Create kfib_struct.cpp

---
+++ kfib_struct.cpp
@@ -0,0 +1,48 @@
+//Rezolvare de 100 de puncte la prob kfib folosind structuri
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+#define MOD %666013
+
+ifstream fin("kfib.in");
+ofstream fout("kfib.out");
+
+struct Matrix {
+	unsigned long long mat[2][2] = { 1, 0, 0, 1 };
+
+	Matrix operator* (Matrix a) {
+		Matrix M;
+		M.mat[0][0] = ((a.mat[0][0] * this-> mat[0][0])MOD + (a.mat[0][1] * this-> mat[1][0])MOD)MOD;
+		M.mat[0][1] = ((a.mat[0][0] * this-> mat[0][1])MOD + (a.mat[0][1] * this-> mat[1][1])MOD)MOD;
+		M.mat[1][0] = ((a.mat[1][0] * this-> mat[0][0])MOD + (a.mat[1][1] * this-> mat[1][0])MOD)MOD;
+		M.mat[1][1] = ((a.mat[1][0] * this-> mat[0][1])MOD + (a.mat[1][1] * this-> mat[1][1])MOD)MOD;
+
+		return M;
+	}
+};
+
+Matrix ridicare(Matrix m, int k) {
+	Matrix ans;
+	while (k) {
+
+		if (k % 2 == 1)
+			ans = ans * m;
+
+		m = m * m;
+		k /= 2;
+	}
+
+	return ans;
+}
+
+int main() {
+	int k;
+	fin >> k;
+	Matrix A;
+	A.mat[0][0] = 0; A.mat[0][1] = A.mat[1][0] = A.mat[1][1] = 1;
+	A = ridicare(A, k);
+
+	fout << A.mat[0][1]MOD;
+	return 0;
+}
