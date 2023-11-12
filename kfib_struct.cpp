0bd20985417330c9709c69228c671a5777a890ab
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-11-12 20:47:29 +0200

    Update kfib_struct.cpp

--- kfib_struct.cpp
+++ kfib_struct.cpp
@@ -1,3 +1,58 @@
+/*DE INTREBAT DE CE NU MERGE
+struct Matrix {
+	long long mat[2][2];
+	
+	Matrix(long long mat[2][2]) : mat() {};
+
+	Matrix() {
+		mat[0][0] = mat[1][1] = 1;
+		mat[1][0] = mat[0][1] = 0;
+	};
+
+	Matrix operator* (Matrix a) {
+		Matrix M;
+		M.mat[0][0] = ((a.mat[0][0] * this-> mat[0][0])MOD + (a.mat[0][1] * this-> mat[1][0])MOD)MOD;
+		M.mat[0][1] = ((a.mat[0][0] * this-> mat[0][1])MOD + (a.mat[0][1] * this-> mat[1][1])MOD)MOD;
+		M.mat[1][0] = ((a.mat[1][0] * this-> mat[0][0])MOD + (a.mat[1][1] * this-> mat[1][0])MOD)MOD;
+		M.mat[1][1] = ((a.mat[1][0] * this-> mat[0][1])MOD + (a.mat[1][1] * this-> mat[1][1])MOD)MOD;
+
+		return M;
+	}
+};
+
+Matrix ridicare(Matrix m, int k) {
+	Matrix ans;
+	while (k) {
+
+		if (k % 2 == 1)
+			ans = ans * m;
+
+		m = m * m;
+		k /= 2;
+	}
+
+	return ans;
+}
+
+int nth_fib(int n) {
+	if (!n) return 0;
+	
+	long long mat[2][2] = { 0, 1, 1, 1 };
+	Matrix fib_matrix = ridicare(Matrix(mat), n--);
+
+	return (fib_matrix.mat[0][0] + fib_matrix.mat[0][1])MOD;
+}
+
+int main() {
+	int k;
+	fin >> k;
+	long long fib_k = nth_fib(k);
+	fout << fib_k;
+
+	return 0;
+}
+*/
+
 //Rezolvare de 100 de puncte la prob kfib folosind structuri
 #include <iostream>
 #include <fstream>
