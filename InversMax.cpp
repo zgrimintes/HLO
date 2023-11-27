145bb8ba1d42a500a0370bb157e5673686738b8f
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-11-27 19:34:06 +0200

    Update InversMax.cpp

--- InversMax.cpp
+++ InversMax.cpp
@@ -1,60 +1,71 @@
-//REZOLVARE DE 60 DE PUNCTE - NU FOLOSI STRTOK!
+//REZOLVARE DE 80 DE PUNCTE
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
-char d[100][100];
-int n, cuv_max = 0;
-
-void revert(char *c) {
+void revert(char* c) {
     int n = strlen(c);
 
-    for (int i = 0; i < n / 2; i++){
+    for (int i = 0; i < n / 2; i++) {
         c[i] = c[i] ^ c[n - i - 1];
         c[n - i - 1] = c[i] ^ c[n - i - 1];
         c[i] = c[i] ^ c[n - i - 1];
     }
 }
 
-void afisare(int n) {
-    for (int i = 0; i < n; i++) {
-        cout << d[i] << " ";
-    }
+bool valid(char* c, int s, int f) {
+    for (s; s < f; s++)
+        if (isalpha(c[s])) return true;
+
+    return false;
 }
 
-bool valid(char *c) {
-    bool ok = false;
-
-    for (int i = 0; i < strlen(c); i++)
-        if (isalpha(c[i])) ok = true;
-
-    return ok;
+void copiere(char *s, char *c, int st, int f) {
+    for (; st < f; st++) {
+        c[st] = s[st];
+    }
 }
 
 int main()
 {
     char s[256];
     cin.getline(s, 256);
+    int cuv_max = 0, index_s = 0, cnt_max = 0;
 
-    char *p = strtok(s, " ");
-
-    while (p) {
-        strcpy(d[n++], p);
-
-        if (cuv_max < strlen(d[n-1]))
-            cuv_max = strlen(d[n-1]);
-
-        p = strtok(NULL, " ");
-    }
-
-    for (int i = 0; i < n; i++) {
-        if (cuv_max == strlen(d[i]) && valid(d[i])) {
-            revert(d[i]);
+    while (index_s < strlen(s)) {
+        if (s[index_s] == ' ') {
+            if (cnt_max > cuv_max) cuv_max = cnt_max;
+            cnt_max = 0;
         }
+        else
+            cnt_max++;
+
+        index_s++;
     }
 
-    afisare(n);
+    if (cuv_max < cnt_max) cuv_max = cnt_max;
+
+    int last_w = 0;
+    char ss[256] = { 0 };
+    for (index_s = 0; index_s <= strlen(s); index_s++) {
+        if (s[index_s] != ' ' && s[index_s] != '\0') continue;
+
+        if (index_s - last_w == cuv_max) {
+            copiere(s, ss, last_w, index_s);
+
+            if (valid(ss, last_w, index_s)) {
+                int offset = 1;
+                for (; last_w < index_s; last_w++) {
+                    s[last_w] = ss[index_s - offset++];
+                }
+            }
+        }
+
+        last_w = index_s + 1;
+    }
+
+    cout << s;
 
     return 0;
 }
