82ee1ba62170d9b1e7c6816e4ffbbd86193c7524
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-12-02 15:51:25 +0200

    Create trifid.cpp

---
+++ trifid.cpp
@@ -0,0 +1,42 @@
+//DE CONTINUAT
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+ifstream fin("trifid.in");
+ofstream fout("trifid.out");
+
+int c;
+char mat[3][3][3];
+char mesaj[10000];
+int* rep;
+
+void citire() {
+	int indxc = 0;
+	int indxl = 0;
+	char c;
+	while (indxl < 3|| indxc != 9) {
+		fin >> c;
+		if (indxc < 3) mat[indxl][indxc++][0] = c;
+		else if (indxc < 6) mat[indxl][indxc++ % 3][1] = c;
+		else if (indxc < 9) mat[indxl][indxc++ % 3][2] = c;
+		else {
+			if (++indxl < 3)
+				mat[indxl][0][0] = c;
+			else
+				continue;
+			indxc = 1;
+		}
+	}
+}
+
+int main() {
+	fin >> c;
+	citire();
+	cin.getline(mesaj, 10000);
+
+	cout << mesaj;
+	return 0;
+}
