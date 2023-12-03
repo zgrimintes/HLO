8cf5ac32517b250472f58acc25b8ae53770ab57d
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-12-03 20:53:24 +0200

    Update trifid.cpp

--- trifid.cpp
+++ trifid.cpp
@@ -17,14 +17,25 @@ void citire() {
 	int indxc = 0;
 	int indxl = 0;
 	char c;
+
 	while (indxl < 3|| indxc != 9) {
-		fin >> c;
-		if (indxc < 3) mat[indxl][indxc++][0] = c;
-		else if (indxc < 6) mat[indxl][indxc++ % 3][1] = c;
-		else if (indxc < 9) mat[indxl][indxc++ % 3][2] = c;
+		if (indxc < 3) {
+			fin >> c;
+			mat[indxl][indxc++][0] = c;
+		}
+		else if (indxc < 6) {
+			fin >> c;
+			mat[indxl][indxc++ % 3][1] = c;
+		}
+		else if (indxc < 9) {
+			fin >> c;
+			mat[indxl][indxc++ % 3][2] = c;
+		}
 		else {
-			if (++indxl < 3)
+			if (++indxl < 3) {
+				fin >> c;
 				mat[indxl][0][0] = c;
+			}
 			else
 				continue;
 			indxc = 1;
@@ -35,7 +46,7 @@ void citire() {
 int main() {
 	fin >> c;
 	citire();
-	cin.getline(mesaj, 10000);
+	fin.getline(mesaj, 10000);
 
 	cout << mesaj;
 	return 0;
