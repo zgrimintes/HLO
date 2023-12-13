fdc469e3b6d8af4bfa7a07507ea98b6f1617c91a
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-12-13 20:07:42 +0200

    Update Expresii.cpp

--- Expresii.cpp
+++ Expresii.cpp
@@ -1,4 +1,4 @@
-//DE TERMINAT
+//REZOLVARE DE 15 PUNCTE
 #include <iostream>
 #include <fstream>
 #include <cstring>
@@ -12,7 +12,7 @@ ofstream fout("expresii.out");
 
 char e[2000005];
 
-int getParanthesis(int dr, int st, char c){
+int getParanthesis(int st, int dr) {
     int cnt = 0;
 
     for (st++; st < dr; st++) {
@@ -24,8 +24,8 @@ int getParanthesis(int dr, int st, char c){
     return -1;
 }
 
-int gcd(int a, int b) {
-    int r = 0;
+long long gcd(int a, int b) {
+    int r = 1;
     while (b) {
         r = a % b;
         a = b;
@@ -35,11 +35,11 @@ int gcd(int a, int b) {
     return a;
 }
 
-int hcf(int a, int b){
+long long hcf(int a, int b) {
     return (a * b) / gcd(a, b);
 }
 
-int getNum(int st, int dr, int x){
+long long getNum(int st, int dr, int x) {
     if (e[st] == 'x') return x;
 
     int num = 0;
@@ -49,21 +49,22 @@ int getNum(int st, int dr, int x){
     return num;
 }
 
-int eval(int st, int dr, int a){
-    int p = getParanthesis(st, dr, '(');
-    if (e[st] == '(') {
-        int E1 = eval(st + 1, p - 1, a);
-        int E2 = eval(p + 1, dr - 1, a);
+long long eval(int st, int dr, int a) {
+    int p = getParanthesis(st, dr);
 
-        return gcd(E1, E2);
-    }
+    if (p != -1) {
+        if (e[st] == '(') {
+            long long E1 = eval(st + 1, p - 1, a);
+            long long E2 = eval(p + 1, dr - 1, a);
 
-    p = getParanthesis(st, dr, '[');
-    if (e[st] == '[') {
-        int E1 = eval(st + 1, p - 1, a);
-        int E2 = eval(p + 1, dr - 1, a);
+            return gcd(E1, E2);
+        }
+        else if (e[st] == '[') {
+            long long E1 = eval(st + 1, p - 1, a);
+            long long E2 = eval(p + 1, dr - 1, a);
 
-        return hcf(E1, E2);
+            return hcf(E1, E2);
+        }
     }
 
     return getNum(st, dr, a);
@@ -73,15 +74,15 @@ int main()
 {
     int l, a, b;
     int sum = 0;
-    cin.getline(e, 2000005);
-    cin >> a >> b;
+    fin.getline(e, 2000005);
+    fin >> a >> b;
     l = strlen(e);
 
     for (; a <= b; a++) {
         sum += eval(0, l - 1, a)MOD;
     }
 
-    cout << sum;
+    fout << sum;
 
     return 0;
 }
