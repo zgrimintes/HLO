27ca2d6d2a1a3d3ed833baf2e7d121c736710b75
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-12-13 20:50:39 +0200

    Update Expresii.cpp

--- Expresii.cpp
+++ Expresii.cpp
@@ -86,3 +86,107 @@ int main()
 
     return 0;
 }
+
+//REZOLVARE FOLOSIND STACK-URI
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <stack>
+
+#define MOD %1000000007
+
+using namespace std;
+
+ifstream fin("expresii.in");
+ofstream fout("expresii.out");
+
+char e[2000005];
+int pos;
+
+stack <char> paranthesis;
+stack <int> nums;
+
+
+long long gcd() {
+    int a = nums.top(); nums.pop();
+    int b = nums.top(); nums.pop();
+    int r = 1;
+    while (b) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+
+long long gcd(int a, int b) {
+    int r = 1;
+    while (b) {
+        r = a % b;
+        a = b;
+        b = r;
+    }
+
+    return a;
+}
+
+long long lcm() {
+    int a = nums.top(); nums.pop();
+    int b = nums.top(); nums.pop();
+    return (a * b) / gcd(a, b);
+}
+
+int getNum(int x) {
+    if (e[pos] == 'x') { pos++; return x; }
+
+    int nb = 0;
+    for (; e[pos] >= '0' && e[pos] <= '9';) {
+        nb = nb * 10 + (e[pos++] - '0');
+    }
+
+    return nb;
+}
+
+int solve(int n, int x) {
+    while (pos < n) {
+        switch (e[pos])
+        {
+        case '(':
+        case '[':
+            paranthesis.push(e[pos++]);
+            break;
+        case ')':
+        case ']':
+            paranthesis.pop();
+            if (e[pos] == ')') nums.push(gcd());
+            else if (e[pos] == ']') nums.push(lcm());
+            pos++;
+            break;
+        case ',':
+            pos++;
+            break;
+        default:
+            nums.push(getNum(x));
+            break;
+        }
+    }
+
+    return nums.top();
+}
+
+int main() {
+    int a, b, sum = 0;
+    fin.getline(e, 2000005);
+    fin >> a >> b;
+
+    for (; a <= b;) {
+       sum = (sum + solve(strlen(e), a++))MOD;
+       nums.pop();
+       pos = 0;
+    }
+
+    fout << sum;
+
+    return 0;
+}
