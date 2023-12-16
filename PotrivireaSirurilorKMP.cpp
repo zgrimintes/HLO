bd42d289db4d97dbed3f633af432a338fcc7bdec
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2023-12-16 20:05:28 +0200

    Create PotrivireaSirurilorKMP.cpp

---
+++ PotrivireaSirurilorKMP.cpp
@@ -0,0 +1,64 @@
+///DE TERMINAT POZITIA DE PE CARE INCEP ASEMANARILE
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+using namespace std;
+
+ifstream fin("strmatch.in");
+ofstream fout("strmatch.out");
+
+char A[2000001], B[2000001];
+int pref[2000001];
+int matches[2000001];
+int cnt_match;
+
+int nA, nB;
+
+void formare_prefix() {
+    int i = 0, j = -1;
+    pref[0] = -1;
+
+    while (i < nA) {
+        if (j >= 0 && A[i] == A[j])
+            j = pref[j];
+
+        j++;
+        i++;
+        pref[i] = j;
+    }
+}
+
+void KMP() {
+    int i = 0, j = -1;
+
+    while (i < nB) {
+        if (j >= 0 && B[i] != B[j])
+            j = pref[j];
+
+        i++;
+        j++;
+        if (j == nA) matches[cnt_match++] = i - j + 1;
+    }
+}
+
+int main()
+{
+    fin.getline(A, 2000001);
+    fin.getline(B, 2000001);
+
+    nA = strlen(A);
+    nB = strlen(B);
+
+    formare_prefix();
+    KMP();
+
+    fout << cnt_match << "\n";
+
+    if (cnt_match > 1000) cnt_match = 1000;
+
+    for (int i = 0; i < cnt_match; i++)
+        fout << matches[i] << " ";
+
+    return 0;
+}
