e347a030335238a97e42ad1b0520ef280111dccf
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-02-02 19:05:13 +0200

    Update and rename DinamicaBottomUp to DinamicaBottomUp.cpp

--- Algoritmi/DinamicaBottomUp.cpp
+++ Algoritmi/DinamicaBottomUp.cpp
@@ -15,10 +15,13 @@ void citPret(){
 
 int solve_bottom_up(){
     for (int i = 2; i <= n; i++) {
+        ///int opt_v = INT_MIN;
         for (int j = 1; j <= i; j++)
             opt[i] = max(opt[i], opt[j] + opt[i - j]);
+            ///opt_v = max(opt_v, pret[i] + opt[j - i])
+        ///opt[i] = opt_v;
     }
-
+    
     return opt[n];
 }
 
