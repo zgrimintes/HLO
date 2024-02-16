e07fd0c492d0b30e5387d8664e002f27ca04e36d
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-02-16 12:13:12 +0200

    Create PODM.cpp

---
+++ Algoritmi/PODM.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <limits.h>
+
+using namespace std;
+
+ifstream fin("podm.in");
+ofstream fout("podm.out");
+
+long long dp[51][51];
+int d[501];
+int n;
+
+void citire(){
+    for (int i = 0; i <= n; i++)
+        fin >> d[i];
+
+}
+
+long long solve(){
+    for (int i = 1; i <= n; i++) dp[i][i] = 0;
+    for (int i = 1; i < n; i++) dp[i][i + 1] = d[i - 1] + d[i] + d[i + 1];
+
+    for (int i = 1; i <= n; i++){
+        int ci = i;
+        for (int j = i + 2; j <= n; j++){
+            dp[ci][j] = LLONG_MAX;
+            for (int k = i; k < j; k++) {
+                dp[ci][j] = min(dp[ci][k] + dp[k + 1][j] + d[i - 1] * d[k] * d[j], dp[ci][j]);
+            }
+
+            ci++;
+        }
+    }
+
+    return dp[1][n];
+}
+
+int main()
+{
+    fin >> n;
+    citire();
+    fout << solve();
+
+    return 0;
+}
