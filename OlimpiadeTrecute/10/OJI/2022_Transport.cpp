91cedefd601859d1bd417b01deacbc6ea000a663
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-02-19 17:20:18 +0200

    Create 2022_Transport.cpp

---
+++ OlimpiadeTrecute/10/OJI/2022_Transport.cpp
@@ -0,0 +1,62 @@
+///REZOLVATA C1 PARTIAl
+#include <fstream>
+
+#define MOD 1000000007
+
+typedef long long LL;
+
+using namespace std;
+
+ifstream fin("transport.in");
+ofstream fout("transport.out");
+
+struct Statie
+{
+	int x, d;
+	LL vD, vS;
+};
+
+Statie s[2000001];
+LL t, n, c;
+LL ind_St;
+LL cnt_rute;
+
+void citire() {
+	int cn = n;
+
+	while (cn) {
+		fin >> s[ind_St].x >> s[ind_St].d;
+		s[ind_St].vD = s[ind_St].x * c - s[ind_St].d;
+		s[ind_St].vS = s[ind_St].x * c + s[ind_St].d;
+		ind_St++;
+		cn--;
+	}
+}
+
+LL solve1() {
+	for (int i = ind_St - 1; i > 1; i--) {
+		for (int j = 0; j < i; j++) {
+			if (s[i].vD != s[j].vS) continue;
+
+			cnt_rute = (cnt_rute + 1) % MOD;
+		}
+	}
+
+	return cnt_rute % MOD;
+}
+
+void solve2() {
+
+}
+
+int main() {
+	fin >> t >> n >> c;
+	citire();
+
+	if (t == 1)
+		fout << solve1();
+	else
+		solve2();
+
+	return 0;
+}
