7f166740f2b2c083b5ef0d083ff15354541aeb7b
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-02-23 19:21:20 +0200

    Update AriaTriunghi.cpp

--- ProblemeLucrate/AriaTriunghi.cpp
+++ ProblemeLucrate/AriaTriunghi.cpp
@@ -15,7 +15,8 @@ Punct p[3];
 float arie;
 
 float det() {
-	return p[0].x * (p[1].y - p[2].y) - p[0].y * (p[1].x - p[2].x) + (p[1].x * p[2].y + p[1].y * p[2].x);
+	//return p[0].x * (p[1].y - p[2].y) - p[0].y * (p[1].x - p[2].x) + (p[1].x * p[2].y + p[1].y * p[2].x);
+	return (p[j].x - p[i].x) * (p[k].y - p[i].y) - (p[k].x - p[i].x) * (p[j].y - p[i].y);
 }
 
 float detArie() {
