fc9fb6df5cb053fed84c769f7783b6df25b4ccf3
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-02-26 19:30:42 +0200

    Update 2020_Rover.cpp

--- OlimpiadeTrecute/10/OJI/2020_Rover.cpp
+++ OlimpiadeTrecute/10/OJI/2020_Rover.cpp
@@ -1,4 +1,3 @@
-///REZOLVARE DE 50 DE PUNCTE - C1
 #include <fstream>
 #include <queue>
 
@@ -13,6 +12,7 @@ int dI[4] = { -1, 0, 1, 0 },
 queue <pair<int, int>> Q;
 int mat[505][505];
 int cost[505][505];
+int viz[505][505];
 int v, n, g;
 int min_dng;
 
@@ -49,16 +49,68 @@ void LEEastDangerous() {
 			int iv = dI[k] + x,
 				jv = dJ[k] + y;
 
-			if (mat[iv][jv] < 0) continue;
+			if (viz[iv][jv] < 0) continue;
 
 			if (cost[iv][jv] == 0 || cost[iv][jv] > (cost[x][y] + isDangerous(mat[iv][jv]))) {
 				cost[iv][jv] = cost[x][y] + isDangerous(mat[iv][jv]);
 				Q.push({ iv, jv });
 
-				if (cost[iv][jv] == 0) mat[iv][jv] = -1;
+				if (cost[iv][jv] == 0) viz[iv][jv] = -1;
 			}
 		}
 	}
+}
+
+void reset() {
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			cost[i][j] = viz[i][j] = 0;
+}
+
+void Lee() {
+	Q.push({ 1, 1 });
+	
+	while (!Q.empty()) {
+		int x = Q.front().first,
+			y = Q.front().second;
+
+		viz[x][y] = -1;
+
+		Q.pop();
+
+		for (int k = 0; k < 4; k++) {
+			int iv = dI[k] + x,
+				jv = dJ[k] + y;
+
+			if (viz[iv][jv] < 0) continue;
+
+			if (!isDangerous(mat[iv][jv])) {
+				Q.push({ iv, jv });
+				viz[iv][jv] = -1;
+			}
+		}
+	}
+}
+
+void getMaximumWeight() {
+	int st = 1, dr = 5000;
+	int g_min = 1;
+
+	while (st <= dr) {
+		g = (st + dr) / 2;
+
+		Lee();
+
+		if (viz[n][n] > -1) dr = g - 1;
+		if (viz[n][n] == -1) {
+			st = g + 1;
+			g_min = min(st, g);
+		}
+
+		reset();
+	}
+
+	g = g_min;
 }
 
 int main() {
@@ -70,7 +122,8 @@ int main() {
 		fout << cost[n][n];
 	}
 	else {
-
+		getMaximumWeight();
+		fout << g;
 	}
 
 	return 0;
