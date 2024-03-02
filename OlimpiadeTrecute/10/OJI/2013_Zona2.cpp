447b9b7b230ecef51477f4a0b4eb67e40d775233
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-03-02 15:21:48 +0200

    Update 2013_Zona2.cpp

--- OlimpiadeTrecute/10/OJI/2013_Zona2.cpp
+++ OlimpiadeTrecute/10/OJI/2013_Zona2.cpp
@@ -1,4 +1,4 @@
-///DE TERMINAT
+///20 PUNCTE - C1
 #include <fstream> 
 
 using namespace std;
@@ -6,24 +6,43 @@ using namespace std;
 ifstream fin("zona2.in");
 ofstream fout("zona2.out");
 
+struct Drum {
+	int x, y;
+};
+
+Drum d[5000];
 int dI[4] = { -1, 0, 1, 0 },
 	dJ[4] = { 0, 1, 0, -1 };
 int mat[55][55], mat_fin[55][55];
-int n, xs, ys, p;
+int n, xs, ys, p, ds = -1;
+
+void srchIntersection(int s) {
+	for (int i = 0; i < s; i++)
+		if (d[i].x == d[s].x && d[i].y == d[s].y)
+			ds = i;
+}
 
 void stephs() {
 	int step;
 	int x = xs,
 		y = ys;
 
-	while (p) {
+	for (int i = 0; i <= p; i++) {
+		d[i].x = x;
+		d[i].y = y;
+
+		if (mat[x][y] == 1) {
+			srchIntersection(i);
+			break;
+		}
 		mat[x][y] = 1;
+
 		fin >> step;
 
 		x += dI[step - 1]; y += dJ[step - 1];
-
-		p--;
 	}
+
+	fout << p - ds;
 }
 
 int main() {
