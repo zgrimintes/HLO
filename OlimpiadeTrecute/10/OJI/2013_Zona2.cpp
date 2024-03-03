bf3216197cc877f9fcb9f8ed0a9e656ceaff3c97
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-03-03 15:21:12 +0200

    Update 2013_Zona2.cpp

--- OlimpiadeTrecute/10/OJI/2013_Zona2.cpp
+++ OlimpiadeTrecute/10/OJI/2013_Zona2.cpp
@@ -1,4 +1,4 @@
-///20 PUNCTE - C1
+///100 DE PUNCTE
 #include <fstream> 
 
 using namespace std;
@@ -14,12 +14,63 @@ Drum d[5000];
 int dI[4] = { -1, 0, 1, 0 },
 	dJ[4] = { 0, 1, 0, -1 };
 int mat[55][55], mat_fin[55][55];
-int n, xs, ys, p, ds = -1;
+int n, xs, ys, p, ds = -1, cnt_pat;
+bool inside = true;
 
 void srchIntersection(int s) {
 	for (int i = 0; i < s; i++)
 		if (d[i].x == d[s].x && d[i].y == d[s].y)
 			ds = i;
+}
+
+void fill(int i, int j) {
+	if (i < 0 || i > n + 1 || j < 0 || j > n + 1) return;
+	if (mat[i][j] == 1) return;
+	if (mat[i][j] == -1) {
+		inside = false;
+		return;
+	}
+
+	cnt_pat++;
+	mat[i][j] = 1;
+
+	if ((mat_fin[i][j] & 1) == 0) fill(i - 1, j);
+	if (((mat_fin[i][j] >> 1) & 1) == 0) fill(i, j + 1);
+	if (((mat_fin[i][j] >> 2) & 1) == 0) fill(i + 1, j);
+	if (((mat_fin[i][j] >> 3) & 1) == 0) fill(i, j - 1);
+}
+
+void moveDir(int dir, int &x, int &y) {
+	x += dI[dir - 1]; y += dJ[dir - 1];
+	
+	switch (dir)
+	{
+	case 1:
+		mat_fin[x + 1][y] += 2;
+		mat_fin[x + 1][y + 1] += 8;
+		break;
+	case 2:
+		mat_fin[x][y] += 4;
+		mat_fin[x + 1][y] += 1;
+		break;
+	case 3:
+		mat_fin[x][y] += 2;
+		mat_fin[x][y + 1] += 8;
+		break;
+	case 4: 
+		mat_fin[x][y + 1] += 4;
+		mat_fin[x + 1][y + 1] += 1;
+		break;
+	}
+}
+
+void bordare_reset() {
+	for (int i = 1; i <= n; i++)
+		for (int j = 1; j <= n; j++)
+			mat[i][j] = 0;
+
+	for (int i = 0; i <= n + 1; i++)
+		mat[i][0] = mat[i][n + 1] = mat[0][i] = mat[n + 1][i] = -1;
 }
 
 void stephs() {
@@ -39,14 +90,28 @@ void stephs() {
 
 		fin >> step;
 
-		x += dI[step - 1]; y += dJ[step - 1];
+		moveDir(step, x, y);
 	}
 
-	fout << p - ds;
+	fout << p - ds << "\n";
+
+	bordare_reset();
 }
 
 int main() {
 	fin >> xs >> ys >> n >> p;
 	stephs();
+	
+	for (int i = 0; i <= n; i++)
+		for (int j = 0; j <= n; j++)
+			if (((mat[i][j] & 1) == 0) && (((mat[i][j] >> 3) & 1) == 0)) {
+				fill(i, j);
+
+				if (inside) fout << cnt_pat;
+				else fout << n * n - cnt_pat;
+
+				return 0;
+			}
+
 	return 0;
 }
