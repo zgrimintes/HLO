6c9356c3d7ee312bbd9f06f31940b0abe89fae98
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-04-27 12:35:41 +0300

    Update BacktrackPermutari.cpp\

--- Algoritmi/BacktrackPermutari.cpp
+++ Algoritmi/BacktrackPermutari.cpp
@@ -36,3 +36,45 @@ int main()
     bkt(n, 0, sir, verif);
     return 0;
 }
+
+///varianta cu std::vector
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void afisare(vector<int> v) {
+    int n = v.size();
+    for (int i = 0; i < n; i++)
+        cout << v[i] << " ";
+
+    cout << "\n";
+}
+
+void bkt(int n, int k, vector<int> v, bool *used) {
+    if (k == n) {
+        afisare(v);
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (used[i]) continue;
+
+        used[i] = true;
+        v.push_back(i + 1); //v[k] = i + 1;
+        bkt(n, k + 1, v, used);
+        used[i] = false;
+        v.pop_back(); //v[k] = 0;
+    }
+
+}
+
+int main()
+{
+    vector<int> sir;
+    bool verif[100] = { 0 };
+    int n;
+    cin >> n;
+    bkt(n, 0, sir, verif);
+    return 0;
+}
