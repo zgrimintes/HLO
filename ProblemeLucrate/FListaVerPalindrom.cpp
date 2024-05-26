751011cbf8006e2f06f2e8695fe3b1b566aff247
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-05-26 20:20:26 +0300

    Update FListaVerPalindrom.cpp

--- ProblemeLucrate/FListaVerPalindrom.cpp
+++ ProblemeLucrate/FListaVerPalindrom.cpp
@@ -1,1 +1,75 @@
+#include <iostream>
 
+using namespace std;
+
+struct Node {
+	int val;
+	Node* adr;
+};
+
+void addToFront(Node* &p, int x) {
+	if (p == 0) {
+		p = new Node;
+		p->val = x;
+		p->adr = 0;
+		return;
+	}
+
+	Node* q = p;
+	while (q->adr) q = q->adr;
+
+	q->adr = new Node;
+	q = q->adr;
+	q->val = x;
+	q->adr = 0;
+}
+
+void addToBack(Node*& p, int x) {
+	if (p == 0) {
+		p = new Node;
+		p->val = x;
+		p->adr = 0;
+		return;
+	}
+
+	Node* q = new Node;
+	q->adr = p;
+	q->val = x;
+	p = q;
+}
+
+bool palindrom(Node* l) {
+	Node* f = new Node;
+	Node* q = l;
+
+	while (q) {
+		addToBack(f, q->val);
+		q = q->adr;
+	}
+
+	while (f && l) {
+		if (f->val != l->val) return false;
+		
+		f = f->adr;
+		l = l->adr;
+	}
+
+	return true;
+}
+
+int main() {
+	int n;
+	cin >> n;
+
+	Node* p;
+	p = NULL;
+
+	while (n) {
+		int x; cin >> x;
+		addToFront(p, x);
+		n--;
+	}
+
+	cout << palindrom(p);
+	return 0;
+}
