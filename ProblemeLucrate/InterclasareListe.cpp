1d04ca30965dd2d76a757529461f1531d7ca536c
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-05-31 12:29:03 +0300

    Update InterclasareListe.cpp

--- ProblemeLucrate/InterclasareListe.cpp
+++ ProblemeLucrate/InterclasareListe.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
+
+ifstream fin("list.in");
 
 struct Node {
     int v;
@@ -25,9 +28,9 @@ void addToFront(Node*& p, int x) {
 }
 
 void citire(int& n, Node*& p) {
-    cin >> n;
+    fin >> n;
     while (n) {
-        int x; cin >> x;
+        int x; fin >> x;
         addToFront(p, x);
         n--;
     }
@@ -35,40 +38,42 @@ void citire(int& n, Node*& p) {
 
 void interclasare(Node*& p1, Node*& p2) {
 
-    //Node *st;
-    //(p1->v > p2->v) ? st = p2, p2 = p2->next: st = p1, p1 = p1->next;
+    if (p1->v > p2->v){
+        swap(p1, p2);
+    }
 
-    while (p1 && p2) {
-        if (p1->v < p2->v) {
-            Node* q = p1->next;
-            p1->next = p2;
-            p1->next->next = q;
+    Node *t1 = p1;
+
+    while (t1 && t1->next && p2) {
+        if (t1->v < p2->v && t1->next->v > p2->v) {
+            Node* q = t1->next;
+            t1->next = new Node;
+            t1->next->v = p2->v;
+            t1->next->next = q;
 
             p2 = p2->next;
-            p1 = p1->next;
+            t1 = t1->next;
         }
-        else p1 = p1->next;
+        else t1 = t1->next;
     }
 
-    while (p2) {
-        Node* q = p1->next;
-        p1->next = new Node;
-        p1->next->v = p2->v;
-        p1->next->next = q;
+    if (p2) t1->next = p2;
+}
 
-        p2 = p2->next;
-        p1 = p1->next;
-    }
+void afisare(Node *p) {
+    for (Node *q = p; q; q = q->next)
+        cout << q->v << " ";
 }
 
 int main()
 {
-    Node* p1 = new Node, * p2 = new Node; 
+    Node* p1 = new Node, * p2 = new Node;
     p1 = p2 = NULL;
 
     int n1, n2;
     citire(n1, p1);
     citire(n2, p2);
     interclasare(p1, p2);
+    afisare(p1);
     return 0;
 }
