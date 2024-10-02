a821f240312f93729b5e0edf36c3c922f94d6295
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-10-02 10:49:30 +0300

    Create Vraja2.cpp

---
+++ ProblemeLucrate/Vraja2.cpp
@@ -0,0 +1,61 @@
+//DE TERMINAT
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[1000];
+int pozMin[1000];
+int cnt_vraji = 0;
+
+void citire() {
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+}
+
+int minArr(int l, int r) {
+    int m = 1000001;
+    for (int i = l; i <= r; i++)
+        m = min(m, arr[i]);
+
+    return m;
+}
+
+void cutDown(int l, int r, int m) {
+    for (int i = l; i <= r; i++)
+        arr[i] -= m;
+}
+
+void vraja(int l, int r) {
+    cnt_vraji++;
+    int m = minArr(l, r);
+    cutDown(l, r, m);
+
+    int indMin = 0;
+
+    for (int i = l; i < r; i++) {
+        if (!arr[i]) {
+            pozMin[indMin++] = i;
+        }
+    }
+
+    int lt = 0;
+    for (int i = 0; i < indMin; i++) {
+        vraja(lt, pozMin[i] - 1);
+
+        lt = pozMin[i] + 1;
+    }
+
+    if (l == r) return;
+
+    vraja(lt, r);
+}
+
+int main()
+{
+    cin >> n;
+    citire();
+    vraja(0, n - 1);
+    cout << cnt_vraji;
+    return 0;
+}
