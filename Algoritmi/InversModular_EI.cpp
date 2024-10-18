deb03666c125f56ae7b4780999cc3f57bd6e1ad0
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-10-18 18:53:13 +0300

    Update InversModular_EI.cpp

--- Algoritmi/InversModular_EI.cpp
+++ Algoritmi/InversModular_EI.cpp
@@ -1,1 +1,41 @@
+#include <fstream> 
 
+using namespace std;
+
+ifstream fin("inversmodular.in");
+ofstream fout("inversmodular.out");
+
+int MOD;
+int a, n;
+
+int gcdExtended(int a, int b, int& x, int& y) {
+	if (a == 0) {
+		x = 0, y = 1;
+		return b;
+	}
+
+	int x1, y1;
+	int gcd = gcdExtended(b % a, a, x1, y1);
+
+	x = y1 - (b / a) * x1;
+	y = x1;
+
+	return gcd;
+}
+
+
+void inverse_mod() {
+	int x, y;
+	int g = gcdExtended(a, n, x, y);
+
+	fout << (x % MOD + MOD) % MOD;
+}
+
+int main() {
+
+	fin >> a >> n;
+	MOD = n;
+	inverse_mod();
+
+	return 0;
+}
