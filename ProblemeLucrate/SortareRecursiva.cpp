c660c91c9ac12c359d2626ecb664c768ccb0689f
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-11-03 23:58:54 +0200

    Update SortareRecursiva.cpp

--- ProblemeLucrate/SortareRecursiva.cpp
+++ ProblemeLucrate/SortareRecursiva.cpp
@@ -1,1 +1,58 @@
 ///Ineficient
+
+#include <iostream>
+
+using namespace std;
+
+void citire(int a[], int n) {
+    if (n == 0) return;
+
+    citire(a, n - 1);
+    cin >> a[n];
+}
+
+void afisare(int a[], int n) {
+    if (n == 0) return;
+
+    afisare(a, n - 1);
+    cout << a[n] << " ";
+}
+
+void sor(int a[], int n, int i, int j)
+{
+    if (i < n)
+    {
+        if (j <= n)
+        {
+            if (a[i] > a[j])
+                swap(a[i], a[j]);
+            sor(a, n, i, j + 1);
+        }
+        else
+        {
+            sor(a, n, i + 1, i + 2);
+        }
+    }
+}
+
+void sortare(int a[], int n, int i, int j) {
+    if (i >= n) return;
+
+    if (j <= n) {
+        if (a[i] > a[j])
+            swap(a[i], a[j]);
+        sortare(a, n, i, j + 1);
+    }
+    else
+        sortare(a, n, i + 1, i + 2);
+}
+
+int main() {
+    int a[100], n;
+    cin >> n;
+    citire(a, n);
+    sortare(a, n, 1, 2);
+    afisare(a, n);
+
+    return 0;
+}
