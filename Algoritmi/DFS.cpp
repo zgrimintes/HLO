a41b2b02c1840b1d5124e04af9c977df54bc0f1a
Tibirean Petru <92109049+zgrimintes@users.noreply.github.com>
2024-11-22 18:11:24 +0200

    Create DFS.cpp

---
+++ Algoritmi/DFS.cpp
@@ -0,0 +1,49 @@
+///DFS pe un graf orientat
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+const int NMAX = 100000;
+
+using namespace std;
+
+ifstream fin("dfs.in");
+ofstream fout("dfs.out");
+
+vector<int> G[NMAX + 1];
+bool viz[NMAX + 1];
+
+int n, m, s;
+
+void citire() {
+	for (int i = 1; i <= m; i++) {
+		int x, y;
+		fin >> x >> y;
+
+		G[x].push_back(y);
+	}
+
+	for (int i = 1; i <= m; i++) {
+		sort(G[i].begin(), G[i].end());
+	}
+}
+
+void dfs(int nod) {
+	if (viz[nod]) return;
+	
+	viz[nod] = 1;
+	fout << nod << " ";
+
+	for (auto varf : G[nod]) {
+		dfs(varf);
+	}
+}
+
+int main() {
+	fin >> n >> m >> s;
+	citire();
+
+	dfs(s);
+
+	return 0;
+}
